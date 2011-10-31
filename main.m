0c92c0bd0e855cc8e47312bdaedf997dd88c8838
BenHall <Ben@Benhall.me.uk>
2011-10-31 17:06:50 +0000

    Basic PhoneCall / CallBack project structure

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloCallBack
+//
+//  Created by Ben Hall on 31/10/2011.
+//  Copyright Red Gate Software 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
